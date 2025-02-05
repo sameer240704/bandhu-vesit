ca72a0f7c70e18542926f4fed380eb381f6f2b24
sameer240704 <sameergupta79711@gmail.com>
2025-02-06 00:42:39 +0530

    Minor fix

--- client/app/api/webhooks/clerk/route.ts
+++ client/app/api/webhooks/clerk/route.ts
@@ -73,7 +73,9 @@ export async function POST(req: Request) {
     const newUser = await createUser(user);
 
     if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
+      const client = await clerkClient();
+
+      await client.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
         },
