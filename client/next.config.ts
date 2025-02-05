106d8b60ac7e443407e11b6dc4953aea5517d069
sameer240704 <sameergupta79711@gmail.com>
2025-02-05 23:26:41 +0530

    Added redirect to /en

--- client/next.config.ts
+++ client/next.config.ts
@@ -16,6 +16,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
