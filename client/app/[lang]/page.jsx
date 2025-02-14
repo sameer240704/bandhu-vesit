40375017318ce71a35967f78463be047d70ffa12
sameer240704 <sameergupta79711@gmail.com>
2025-02-14 21:06:40 +0530

    Added Voice Navigation

--- client/app/[lang]/page.jsx
+++ client/app/[lang]/page.jsx
@@ -1,13 +1,16 @@
 import Navbar from "@/components/Home/Navbar";
+import VoiceControl from "@/components/Misc/VoiceControl";
 
 export default function Home() {
   return (
-    <div className="h-screen" suppressHydrationWarning>
+    <div className="relative h-screen" suppressHydrationWarning>
       <Navbar />
 
       <main className="h-full mx-auto px-5 flex-center">
         <h1 className="text-3xl font-semibold">Hello World</h1>
       </main>
+
+      <VoiceControl />
     </div>
   );
 }
