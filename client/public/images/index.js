0f2253dab1cbdd41c7258a6e9f4d5b6ec42cfe03
sameer240704 <sameergupta79711@gmail.com>
2025-02-16 02:42:10 +0530

    Added hi and mr

--- client/public/images/index.js
+++ client/public/images/index.js
@@ -12,7 +12,6 @@ import AvatarBuilder from "./avatar-builder.jpeg";
 import Wall1 from "./wall1.png";
 import Coins from "./coins.png";
 import background from "./whiteboard_bg.avif";
-import Wall1 from "./wall1.png";
 
 export {
   FlappyBird,
