eaac32b91c18f63892a0584505bac4d2223a378b
sameer240704 <sameergupta79711@gmail.com>
2025-02-16 04:03:50 +0530

    Added Bandhu Universe

---
+++ client/app/[lang]/(mindplay)/games/universe/bandhu-universe/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BandhuUniversePage = () => {
+  return (
+    <div className="h-full w-full border border-black rounded-lg overflow-hidden">
+      <iframe src="http://localhost:5173/" width="100%" height="100%"></iframe>
+    </div>
+  );
+};
+
+export default BandhuUniversePage;
--- client/app/[lang]/(mindplay)/games/universe/world/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-const WorldPage = () => {
-  return <div className="h-full w-full border border-black rounded-xl"></div>;
-};
-
-export default WorldPage;
